83d475e113562dd49dc111e1eb64286735e4b8ea
mohan145 <mohan145krishna@gmail.com>
2021-02-20 20:40:55 +0530

    added user model

---
+++ urbanClapBackend/user/constants/enums.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class UserType(Enum):
+    CLIENT = "CLIENT"
+    ADMIN = "ADMIN"
+    SERVICE_PROVIDER = "SERVICE_PROVIDER"
+
--- urbanClapBackend/user/models.py
+++ urbanClapBackend/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# import
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=20)
+    address = models.CharField(max_length=500)
+    locality = models.CharField(max_length=20, default=None)
+    availability = models.BooleanField()
+    service_type = models.CharField(max_length=20)
+    user_type = models.CharField(max_length=20, validators=[])
