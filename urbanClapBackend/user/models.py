0a557b014138b92d617a50de86d2ec827bcc06e3
mohan145 <mohan145krishna@gmail.com>
2021-02-20 21:44:52 +0530

    implemented user/ceate

--- urbanClapBackend/user/models.py
+++ urbanClapBackend/user/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# import
 
 class User(models.Model):
     name = models.CharField(max_length=100)
--- urbanClapBackend/user/views.py
+++ urbanClapBackend/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+from serializers import UserSerializer
+
+class UserViewSet(viewsets.ViewSet):
+
+    def create(self, request):
+        userSer = UserSerializer(data=request.data)
+        userSer.is_valid(raise_exception=True)
+        userSer.save()
+        return Response(userSer.data,status=status.HTTP_200_OK)
