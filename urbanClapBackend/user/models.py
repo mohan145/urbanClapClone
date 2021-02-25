f1d596c2947bab71c43ca98949e4b08fafa362ff
Kavya Kurumaddali <kavya.kurumaddali@nagarro.com>
2021-02-25 23:28:22 +0530

    implemented user apis

--- urbanClapBackend/user/models.py
+++ urbanClapBackend/user/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     phone_no = models.CharField(max_length=20)
     address = models.CharField(max_length=500)
-    locality = models.CharField(max_length=20, default=None)
+    locality = models.CharField(max_length=20, default=None, null=True)
     availability = models.BooleanField()
     service_type = models.CharField(max_length=20)
     user_type = models.CharField(max_length=20, validators=[])
